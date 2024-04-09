2b27c0cb7fd2cfbb2cbb26f9640d52038cd19fb7
datanh98hp <dat198hp@gmail.com>
2024-04-09 11:57:24 +0700

    esline disable : react-hooks/exhaustive-deps: off

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,11 @@ export default function Home() {
       <div className="bg-cube-banner bg-cover bg-clip-content bg-center h-screen w-full">
         <div className="flex h-full flex-col md:flex-row gap-10 justify-center items-center">
           <TypewriterEffectSmoothContainer
-            text="Wellcome to Developer's Đỗ Văn Đạt ja"
+            text="Wellcome to Developer's Đỗ Văn Đạt 98"
             highLineWord="Đỗ Văn Đạt"
             highLineClass="text-white-500 dark:text-blue-500"
           />
+
         </div>
       </div>
       <NavbarMenu />
