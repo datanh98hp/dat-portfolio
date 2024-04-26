95e24eea84cc3fe054fc88801ae0095bc7f11e2d
datanh98hp <dat198hp@gmail.com>
2024-04-26 10:54:40 +0700

    fix send email with url

--- lib/nodemail/sendmail.ts
+++ lib/nodemail/sendmail.ts
@@ -1,16 +1,6 @@
 
 import nodemailer from 'nodemailer';
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        // user: process.env.EMAIL,
-        // pass: process.env.EMAIL_PASS
-        user: "dat198hp@gmail.com",
-        pass: "oryd kskb bnbm amxw" 
-        //oryd kskb bnbm amxw
 
-    }
-});
 interface EmailContent {
     from: string;
     to: string;
@@ -20,6 +10,22 @@ interface EmailContent {
 }
 export const  sendEmail = async (
     { from, to, subject, text, html }: EmailContent) => {
+
+    const transporter = nodemailer.createTransport({
+        port: 465,
+        // service: 'gmail',
+        host: "smtp.gmail.com",
+        auth: {
+            // user: process.env.EMAIL,
+            // pass: process.env.EMAIL_PASS
+            user: "dat198hp@gmail.com",
+            pass: "oryd kskb bnbm amxw"
+            //oryd kskb bnbm amxw
+
+        },
+        secure: true
+    });
+
     //send email
     transporter.sendMail({
         from,
